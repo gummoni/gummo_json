4b82e6d577f0190cf7d517bc1afc453071b61330
K <shaty3@hotmail.com>
2018-12-20 16:39:11 +0900

    first commit.

--- script_basic/rpn.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RPN_H__
-#define __RPN_H__
-
-extern void rpn_reset(void);
-extern void rpn_decode(int value);
-extern int rpn_result(void);
-
-#endif//__RPN_H__
--- script_basic/script_decoder.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SCRIPT_DECODER_H__
-#define __SCRIPT_DECODER_H__
-
-extern int decoder_execute(void);
-
-#endif//__SCRIPT_DECODER_H__
